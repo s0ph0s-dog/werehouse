c8a3174045f4b774c3c3ae5fafb8c43ceaa13fd6
s0ph0s <awoo@s0ph0s.dog>
2024-04-19 22:21:57 -0400

    Add tests for e621 scraper, refactor common parts out

--- test/test.lua
+++ test/test.lua
@@ -102,6 +102,9 @@ TestFunctools = {}
         end
     end
 
+---@alias MockData {whenCalledWith: string, thenReturn: any[]}
+
+---@param data MockData[]
 local function fetch_mock(data)
     local orig = Fetch
     Fetch = function(url, opts)
@@ -117,6 +120,8 @@ local function fetch_mock(data)
     return orig
 end
 
+---@param url string
+---@return MockData
 local function fetch_mock_head_html_200(url)
     return {
         whenCalledWith = url,
@@ -128,6 +133,36 @@ local function fetch_mock_head_html_200(url)
     }
 end
 
+---@alias ProcessEntryTest { input: ActiveQueueEntry, expected: { 1: EntryTask, 2: ScraperError } }
+
+---@param test_data ProcessEntryTest[]
+---@param mocks MockData[]
+local function process_entry_framework(test_data, mocks)
+    local original = fetch_mock(mocks)
+    local results = {}
+    for _, test in ipairs(test_data) do
+        local result, errmsg = pipeline.process_entry(test.input)
+        table.insert(results, {
+            input = test.input,
+            expected = test.expected,
+            output = { result, errmsg }
+        })
+    end
+    -- Must do this before asserting so that I don't leave global state messed up
+    Fetch = original
+    for _, result in ipairs(results) do
+        luaunit.assertEquals(
+            result.output[2],
+            result.expected[2],
+            "error mismatch for input: %s" % {result.input}
+        )
+        luaunit.assertEquals(
+            result.output[1],
+            result.expected[1],
+            "output mismatch for input: %s" % {result.input}
+        )
+    end
+end
 
 TestScraperPipeline = {}
 
@@ -161,88 +196,90 @@ TestScraperPipeline = {}
 
     function TestScraperPipeline:testValidBskyLink()
         local input = { link = "https://bsky.app/profile/did:plc:4gjc5765wbtvrkdxysyvaewz/post/3kphxqgx6iv2b" }
-        local original = fetch_mock{
+        local mocks = {
             fetch_mock_head_html_200("https://bsky.app/profile/did:plc:4gjc5765wbtvrkdxysyvaewz/post/3kphxqgx6iv2b"),
             {
                 whenCalledWith="https://bsky.social/xrpc/com.atproto.repo.getRecord?repo=did%3Aplc%3A4gjc5765wbtvrkdxysyvaewz&collection=app.bsky.feed.post&rkey=3kphxqgx6iv2b",
-                thenReturn={200, {}, [[{"uri":"at://did:plc:4gjc5765wbtvrkdxysyvaewz/app.bsky.feed.post/3kphxqgx6iv2b","cid":"bafyreiaawqoyfcyqd34vybfq3lvwb7luew3rijslyjmfquoy3m2lnvzaqu","value":{"text":"People often ask Pastel how he knows that Constellation actually exists. Though they often don't accept \"I personally know her\" as an answer.","$type":"app.bsky.feed.post","embed":{"$type":"app.bsky.embed.images","images":[{"alt":"Constellation, the god of the universe pastel lives in, and Pastel having sex outside at some ruins.","image":{"$type":"blob","ref":{"$link":"bafkreib2v6upf5gz7q22jpdnrh2fwhtn6yexrsnbp6uh7ythgq3obhf7ia"},"mimeType":"image/jpeg","size":523864},"aspectRatio":{"width":1905,"height":2000}},{"alt":"Same as before but pastel is cumming.","image":{"$type":"blob","ref":{"$link":"bafkreidjkqudkq2m6pojavuelcud2fez2eojxiflnxedimplumiygu76pe"},"mimeType":"image/jpeg","size":523698},"aspectRatio":{"width":1905,"height":2000}}]},"langs":["en"],"labels":{"$type":"com.atproto.label.defs#selfLabels","values":[{"val":"porn"}]},"createdAt":"2024-04-06T15:42:51.710Z"}}]]}
+                thenReturn={200, {}, Slurp("./test/bsky_example.json")}
             }
         }
-        local result, errmsg = pipeline.process_entry(input)
-        Fetch = original
         local expected = { archive = {
-                {
-                    height=2000,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://cdn.bsky.app/img/feed_thumbnail/plain/did:plc:4gjc5765wbtvrkdxysyvaewz/bafkreib2v6upf5gz7q22jpdnrh2fwhtn6yexrsnbp6uh7ythgq3obhf7ia@jpeg",
-                    width=1905
-                },
-                {
-                    height=2000,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://cdn.bsky.app/img/feed_thumbnail/plain/did:plc:4gjc5765wbtvrkdxysyvaewz/bafkreidjkqudkq2m6pojavuelcud2fez2eojxiflnxedimplumiygu76pe@jpeg",
-                    width=1905
-                }
+            {
+                height=2000,
+                mime_type="image/jpeg",
+                raw_image_uri="https://cdn.bsky.app/img/feed_thumbnail/plain/did:plc:4gjc5765wbtvrkdxysyvaewz/bafkreib2v6upf5gz7q22jpdnrh2fwhtn6yexrsnbp6uh7ythgq3obhf7ia@jpeg",
+                width=1905
+            },
+            {
+                height=2000,
+                mime_type="image/jpeg",
+                raw_image_uri="https://cdn.bsky.app/img/feed_thumbnail/plain/did:plc:4gjc5765wbtvrkdxysyvaewz/bafkreidjkqudkq2m6pojavuelcud2fez2eojxiflnxedimplumiygu76pe@jpeg",
+                width=1905
+            }
         }}
-        luaunit.assertEquals(result, expected)
-        luaunit.assertIsNil(errmsg)
+        local tests = {
+            { input = input, expected = { expected, nil } },
+        }
+        process_entry_framework(tests, mocks)
     end
 
     function TestScraperPipeline:testValidTwitterLinks()
         local tweetTrackingParams = "https://twitter.com/thatFunkybun/status/1778885919572979806?s=19"
         local tweetVxtwitter = "https://vxtwitter.com/thatFunkybun/status/1778885919572979806"
         local tweetNitter = "https://nitter.privacydev.net/thatFunkybun/status/1778885919572979806#m"
-        local inputs = table.map(
-            {tweetTrackingParams, tweetVxtwitter, tweetNitter},
-            function (i) return { link = i } end
-        )
-        local original = fetch_mock{
+        local mocks = {
             fetch_mock_head_html_200(tweetTrackingParams),
             fetch_mock_head_html_200(tweetVxtwitter),
             fetch_mock_head_html_200(tweetNitter),
             {
                 whenCalledWith = "https://api.fxtwitter.com/status/1778885919572979806",
-                thenReturn = {200, {}, [[{"code":200,"message":"OK","tweet":{"url":"https://twitter.com/thatFunkybun/status/1778885919572979806","id":"1778885919572979806","text":"each second month I spend some time making an exclusive project for the €5 patrons, and this is that project from november! now released for the public.\nsorry I took so long on this month's exclusive X) it's only 14 pages but still took me 3 weeks","author":{"id":"986175872515362816","name":"Funkybun","screen_name":"thatFunkybun","avatar_url":"https://pbs.twimg.com/profile_images/1466055016880422925/TrD9-bqQ_200x200.jpg","banner_url":"https://pbs.twimg.com/profile_banners/986175872515362816/1638369730","description":"I am Funkybun (she/her), making NSFW exhibitionism art for all of you! \nyou can find my art over at:\nhttp://patreon.com/funkybun","location":"","url":"https://twitter.com/thatFunkybun","followers":77430,"following":31,"joined":"Tue Apr 17 09:33:45 +0000 2018","likes":2606,"website":{"url":"https://www.patreon.com/funkybun","display_url":"patreon.com/funkybun"},"tweets":828,"avatar_color":null},"replies":34,"retweets":690,"likes":4048,"created_at":"Fri Apr 12 20:40:27 +0000 2024","created_timestamp":1712954427,"possibly_sensitive":true,"views":80815,"is_note_tweet":false,"lang":"en","replying_to":null,"replying_to_status":null,"media":{"all":[{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDarXQAE6yBj.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaaXsAATM_X.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaUWYAABb40.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaUXsAAGJng.jpg","width":1600,"height":2300,"altText":""}],"photos":[{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDarXQAE6yBj.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaaXsAATM_X.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaUWYAABb40.jpg","width":1600,"height":2300,"altText":""},{"type":"photo","url":"https://pbs.twimg.com/media/GK_fDaUXsAAGJng.jpg","width":1600,"height":2300,"altText":""}],"mosaic":{"type":"mosaic_photo","formats":{"jpeg":"https://mosaic.fxtwitter.com/jpeg/1778885919572979806/GK_fDarXQAE6yBj/GK_fDaaXsAATM_X/GK_fDaUWYAABb40/GK_fDaUXsAAGJng","webp":"https://mosaic.fxtwitter.com/webp/1778885919572979806/GK_fDarXQAE6yBj/GK_fDaaXsAATM_X/GK_fDaUWYAABb40/GK_fDaUXsAAGJng"}}},"source":"Twitter Web App","twitter_card":"summary_large_image","color":null}}]]}}
+                thenReturn = {200, {}, Slurp("test/twitter_fxtwitter_response.json")}}
         }
-        local expected = {
-            archive = {
-                {
-                    height=2300,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://pbs.twimg.com/media/GK_fDarXQAE6yBj.jpg",
-                    width=1600
-                },
-                {
-                    height=2300,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://pbs.twimg.com/media/GK_fDaaXsAATM_X.jpg",
-                    width=1600
-                },
-                {
-                    height=2300,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://pbs.twimg.com/media/GK_fDaUWYAABb40.jpg",
-                    width=1600
-                },
-                {
-                    height=2300,
-                    mime_type="image/jpeg",
-                    raw_image_uri="https://pbs.twimg.com/media/GK_fDaUXsAAGJng.jpg",
-                    width=1600
-                },
-            }
+        local expected = { archive = {
+            {
+                height=2300,
+                mime_type="image/jpeg",
+                raw_image_uri="https://pbs.twimg.com/media/GK_fDarXQAE6yBj.jpg",
+                width=1600
+            },
+            {
+                height=2300,
+                mime_type="image/jpeg",
+                raw_image_uri="https://pbs.twimg.com/media/GK_fDaaXsAATM_X.jpg",
+                width=1600
+            },
+            {
+                height=2300,
+                mime_type="image/jpeg",
+                raw_image_uri="https://pbs.twimg.com/media/GK_fDaUWYAABb40.jpg",
+                width=1600
+            },
+            {
+                height=2300,
+                mime_type="image/jpeg",
+                raw_image_uri="https://pbs.twimg.com/media/GK_fDaUXsAAGJng.jpg",
+                width=1600
+            },
+        } }
+        local tests = {
+            {
+                input = { link = tweetTrackingParams },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = tweetTrackingParams },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = tweetVxtwitter },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = tweetNitter },
+                expected = { expected, nil }
+            },
         }
-        local results = {}
-        for _, input in ipairs(inputs) do
-            local result, errmsg = pipeline.process_entry(input)
-            table.insert(results, {input, result, errmsg})
-        end
-        -- Must do this before asserting so that I don't leave global state messed up
-        Fetch = original
-        for _, result in ipairs(results) do
-            luaunit.assertIsNil(result[3], "input: %s" % {result[1]})
-            luaunit.assertEquals(result[2], expected, "input: %s" % {result[1]})
-        end
+        process_entry_framework(tests, mocks)
     end
 
     function TestScraperPipeline:testValidFuraffinityLinks()
@@ -250,11 +287,7 @@ TestScraperPipeline = {}
         local inputFx = "https://www.fxfuraffinity.net/view/36328438"
         local inputX = "https://www.xfuraffinity.net/view/36328438"
         local inputNoWwwFull = "https://furaffinity.net/full/36328438"
-        local inputs = table.map(
-            {inputRegular, inputFx, inputX, inputNoWwwFull},
-            function (i) return { link = i } end
-        )
-        local original = fetch_mock{
+        local mocks = {
             fetch_mock_head_html_200(inputRegular),
             fetch_mock_head_html_200(inputFx),
             fetch_mock_head_html_200(inputX),
@@ -264,27 +297,94 @@ TestScraperPipeline = {}
                 thenReturn = {200, {}, Slurp("./test/fa_example.html")},
             },
         }
-        local expected = {
-            archive = {
-                {
+        local expected = { archive = { {
                     height=1280,
                     mime_type="image/png",
                     raw_image_uri="https://d.furaffinity.net/art/glopossum/1589320262/1589320262.glopossum_chloelatex.png",
                     width=960,
-                }
+        } } }
+        local tests = {
+            {
+                input = { link = inputRegular },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = inputFx },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = inputX },
+                expected = { expected, nil }
+            },
+            {
+                input = { link = inputNoWwwFull },
+                expected = { expected, nil }
+            },
+        }
+        process_entry_framework(tests, mocks)
+    end
+
+    function TestScraperPipeline:testValidE6Links()
+        local inputRegular = "https://e621.net/posts/4366241"
+        local inputQueryParams = "https://e621.net/posts/4366241?q=filetype%3Ajpg+order%3Ascore"
+        local inputQueryParamsWithJson = "https://e621.net/posts/4366241.json?q=filetype%3Ajpg%2Border%3Ascore"
+        local expectedRegular = {
+            archive = {
+                height = 1100,
+                mime_type = "image/jpeg",
+                raw_image_uri = "https://static1.e621.net/data/63/f2/63f28a75d91d42252326235a03efe93e.jpg",
+                width = 880,
             }
         }
-        local results = {}
-        for _, input in ipairs(inputs) do
-            local result, errmsg = pipeline.process_entry(input)
-            table.insert(results, {input, result, errmsg})
-        end
-        -- Must do this before asserting so that I don't leave global state messed up
-        Fetch = original
-        for _, result in ipairs(results) do
-            luaunit.assertIsNil(result[3], "input: %s" % {result[1]})
-            luaunit.assertEquals(result[2], expected, "input: %s" % {result[1]})
-        end
+        local inputVideo = "https://e621.net/posts/2848682"
+        local inputGif = "https://e621.net/posts/3105830"
+        local tests = {
+            {
+                input = { link = inputRegular },
+                expected = { expectedRegular, nil },
+            },
+            {
+                input = { link = inputQueryParams },
+                expected = { expectedRegular, nil },
+            },
+            {
+                input = { link = inputVideo },
+                expected = { nil, PermScraperError("This post is a webm, which isn't supported yet") },
+            },
+            {
+                input = { link = inputGif },
+                expected = { { archive = {
+                    height = 1920,
+                    mime_type = "image/gif",
+                    raw_image_uri = "https://static1.e621.net/data/f6/d9/f6d9af24b4a47fd324bd41ebe21aeb42.gif",
+                    width = 1080,
+                }}, nil}
+            }
+        }
+        local regular_response_body = Slurp("test/e6_regular_example.json")
+        local mocks = {
+            fetch_mock_head_html_200(inputRegular),
+            fetch_mock_head_html_200(inputQueryParams),
+            fetch_mock_head_html_200(inputVideo),
+            fetch_mock_head_html_200(inputGif),
+            {
+                whenCalledWith = inputRegular .. ".json",
+                thenReturn = {200, {}, regular_response_body},
+            },
+            {
+                whenCalledWith = inputQueryParamsWithJson,
+                thenReturn = {200, {}, regular_response_body},
+            },
+            {
+                whenCalledWith = inputVideo .. ".json",
+                thenReturn = {200, {}, Slurp("test/e6_video_example.json")},
+            },
+            {
+                whenCalledWith = inputGif .. ".json",
+                thenReturn = {200, {}, Slurp("test/e6_gif_example.json")},
+            },
+        }
+        process_entry_framework(tests, mocks)
     end
 
 luaunit.run()
